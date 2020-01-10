3c5e79372e684e096ebb2b4b20de89fb283ce8a3
Filipe A Moreira <code@filipeamoreira.com>
2020-01-10 00:33:34 +0000

    Cleanup after ourselves

--- app/jobs/generate_montage_job.rb
+++ app/jobs/generate_montage_job.rb
@@ -4,7 +4,8 @@ class GenerateMontageJob < ApplicationJob
   LOGO_FILENAME = 'generic_logo.png'.freeze
   LOGO_FILEPATH = Rails.root.join("public/#{LOGO_FILENAME}")
 
-def perform(image_urls)
+  def perform(image_urls)
+    random_sufix = rand(0..100) # Add this to the temp file name to avoid collision
     # Create image records for each image url
     @montage = ImageMontage.new
     image_urls.each do |url|
@@ -18,17 +19,25 @@ def perform(image_urls)
     MiniMagick::Tool::Montage.new do |montage|
       montage.geometry 'x700+0+0'
       @montage.images.each { |i| montage << i.url }
-      montage << Rails.root.join('tmp/raw.jpg')
+      montage << Rails.root.join("tmp/raw-#{random_sufix}.jpg")
     end
 
     # Add logo watermark
-    processed_image = MiniMagick::Image.open(Rails.root.join('tmp/raw.jpg'))
+    processed_image = MiniMagick::Image.open(Rails.root.join("tmp/raw-#{random_sufix}.jpg"))
     processed_image.combine_options do |c|
       c.gravity 'SouthEast'
       c.draw "image Over 0,0,0,0 \"#{LOGO_FILEPATH}\""
     end
-    processed_image.write(Rails.root.join('tmp/montage.jpg'))
+    processed_image.write(Rails.root.join("tmp/montage-#{random_sufix}.jpg"))
     @montage.file.attach(io: File.open(processed_image.path), filename: 'montage.jpg')
     @montage.save!
+    cleanup_files(random_sufix)
+  end
+
+  private
+
+  def cleanup_files(random_sufix)
+    File.delete(Rails.root.join("tmp/raw-#{random_sufix}.jpg"))
+    File.delete(Rails.root.join("tmp/montage-#{random_sufix}.jpg"))
   end
 end
